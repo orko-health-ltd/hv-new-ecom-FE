aa100e970ef8da7469842afa6f3c8c2c16d35e58
Ishmam Bin Azim <ishmamazim2@gmail.com>
2025-02-22 14:46:53 +0600

    added ecosystem file for mjs support

---
+++ ecosystem.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nuxt-app',
+      script: '.output/server/index.mjs',
+      instances: 'max', // Adjust instances as needed
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 4000, // Change port as needed
+      },
+      node_args: '--experimental-specifier-resolution=node',
+    },
+  ],
+}

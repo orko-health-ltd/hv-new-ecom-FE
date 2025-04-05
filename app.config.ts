29eaf4810c45ed74ae2f1103b8a1e912a5fd5bb7
Ishmam <ishmamazim2@gmail.com>
2025-04-05 10:58:12 +0600

    dark mode and order placement

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ export default defineAppConfig({
     notifications: {
       position: 'top-0 bottom-[unset]',
     },
-    primary: 'blue', // or any color you use
+    primary: 'green', // or any color you use
     gray: 'cool', // optional
     mode: 'light', // ✅ force light mode,
     
